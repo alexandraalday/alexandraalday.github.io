04c40847895127e0de2871bd2e33bb148c215505
alexandraalday <alexandra.alday@gmail.com>
2017-09-16 23:38:46 -0700

    portfolio slider with slick

--- js/magic-spells.js
+++ js/magic-spells.js
@@ -1,29 +1,74 @@
-$(() => {
 
-	$(document).load(function() {
 
-	let fadein_tween = TweenMax.to('#portfolio-trigger > div', 1,{ opacity: 1 });
-	let fadeout_tween = TweenMax.to('#portfolio-trigger > div', 1,{ opacity: 0 });
+$(window).on("load",function(){
 
-	let controller = new ScrollMagic.Controller();
+$(function() {
 
-	let fadein_scene = new ScrollMagic.Scene({
-	  triggerElement: '#portfolio-trigger',
-	  reverse: true
-	})
-	.setTween(fadein_tween)
-	.addTo(controller);
 
-	let fadeout_scene = new ScrollMagic.Scene({
-	  triggerElement: '#portfolio-trigger',
-	  reverse: true
-	})
-	.setTween(fadeout_tween)
-	.addTo(controller);
+	$('.slider-portfolio-pic').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-portfolio-nav'
+	});
+
+
+	$('.slider-portfolio-content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+		asNavFor: '.slider-portfolio-pic',       
+        slide: 'div',
+		autoplay:true,
+		arrows: false
+	});
+
+
+	$('.slider-portfolio-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 1000,
+        asNavFor: '.slider-portfolio-content,.slider-portfolio-pic',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true,
+        slide: 'div',
+		autoplay:true,
+      responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        arrows: true,
+	        centerMode: true,
+	        centerPadding: '40px',
+	        slidesToShow: 3
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        arrows: true,
+	        centerMode: true,
+	        centerPadding: '40px',
+	        slidesToShow: 2
+	      }
+	    }]
+	});    
+  
+	//typed.js 
+	$(function(){
+		$("#typed").typed({
+			stringsElement: $('#typed-strings'),
+	        typeSpeed: 80      
+	      });	  
+	});
 
 });
 
 });
+
 
 
 // Open the console...
